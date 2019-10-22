00466ed1ba93d40ac949a38bd99fbd7622c10b5a
Adam Norton <adam@adamnorton.com>
2019-10-22 16:18:36 -0500

    initial add of files and express server is listening.

---
+++ app/server.js
@@ -0,0 +1,39 @@
+//=================================================
+// Dependencies
+//=================================================
+var express = require('express');
+var path = require('path');
+
+
+//=================================================
+// Tells node we are creating an Express server
+//=================================================
+var app = express();
+
+
+//=================================================
+// Sets initial port. Used later in listener.
+//=================================================
+var PORT = process.env.PORT || 8080;
+
+
+//=================================================
+// Sets up Express app to handle data parsing
+//=================================================
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+//=================================================
+// Routing
+//=================================================
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+//=================================================
+// Listener
+//=================================================
+app.listen(PORT, function(){
+    console.log(`App listening on PORT: ${PORT}`);
+});
