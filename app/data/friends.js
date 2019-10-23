ca8431f38a7f5fd2a7017fb215a2fc0cb5c35138
Adam Norton <adam@adamnorton.com>
2019-10-22 19:23:16 -0500

    added api routes and data to friends array.

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+// DATA
+//===================================
+
+var friendsArray = [
+    {
+        name: "Eden Bond",
+        photo: "https://images.squarespace-cdn.com/content/v1/5a9712e4e17ba3c23a7eaa28/1519865240400-3I2WV33C5ZAWUZQV6MEB/ke17ZwdGBToddI8pDm48kJ9hxOmVac89_BSWidqtaI4UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z24Y84JT1lUs7RW7f0QXDhROJYMABIn8547arD2JsWCDJbTvJybZOtvbWRiFosmN10/_DSC6092-Edit.jpg?format=1000w",
+        scores: [
+            5,1,3,4,2,3,4,2,5,1
+        ]
+    },
+    {
+        name: "Veronica Lizarraga",
+        photo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/41697228_10210094483318069_1074405576835858432_n.jpg?_nc_cat=110&_nc_oc=AQmgRcWI1lkNmGAq1ZlWgVnioSQvS-Rzvuyga9I64_QpaTvySP2797KxPyiQ5YwjOAQ&_nc_ht=scontent-dfw5-1.xx&oh=c0a82f652e8880f00a7f3afcc63ed7b6&oe=5E1C7956",
+        scores: [
+            2,4,5,5,3,4,1,2,3,5
+        ]
+    },
+    {
+        name: "Danilo Buendia",
+        photo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13600244_10153232121872185_7317201170171460790_n.jpg?_nc_cat=107&_nc_oc=AQn1Ow7ZHI7FLHCVIhmLW3hHmjTrQfURt2EGCOABXWxPB9WhSXqkFg36sS6y-ZTa5M8&_nc_ht=scontent-dfw5-1.xx&oh=d703b1073934bbb8a8be21d499ffaa90&oe=5E227F51",
+        scores: [
+            2,3,4,5,5,4,3,2,1,2
+
+        ]
+    },
+    {
+        name: "Brandi Cooling",
+        photo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/55833042_10218834361430653_6624674968871370752_n.jpg?_nc_cat=109&_nc_oc=AQkj82ddkPlRhxlcFilYx_O7n50ORcaZZHDARnexHafmFAgerdbiCCxSePp3cblSq3M&_nc_ht=scontent-dfw5-1.xx&oh=c2cb2cdf20143455f322fb73ab5f8be5&oe=5E636532",
+        scores: [
+            1,3,2,4,5,3,4,2,5,3
+        ]
+    },
+    {
+        name: "Dave Mueller",
+        photo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13327569_10207790329211047_3759448462604294491_n.jpg?_nc_cat=105&_nc_oc=AQnTxiCDTJ0Sop-j4obDN89-BxEQYOpzPDE8OCcgPCfdyvkK6bpDdGHK9STJ5D9Q_AQ&_nc_ht=scontent-dfw5-1.xx&oh=b1cfdff11aa7add1487c307248f558ed&oe=5E2007D7",
+        scores: [
+            5,5,5,4,4,4,3,3,3,2
+        ]
+    },
+
+];
+
+var user = {
+    name: "",
+    photo: "",
+    scores: []
+};
+
+
+module.exports = friendsArray;
+module.exports = user;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,25 @@
+// Load Data
+//===========================================
+var friends = require("../data/friends");
+
+
+
+//============================================
+// Routing
+//=============================================
+
+module.exports = function(app){
+    
+    app.get("api/friends", function(req, res){
+        res.json(friends);
+    });
+
+    app.post("api/friends", function(req, res){
+        friends.user.push(req.body);
+
+        
+        
+
+    });
+};
+
